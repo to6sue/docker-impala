531c272c824d6e40c65ed0575bdcf159743637e5
root <root@ip-172-31-101-38.ap-northeast-2.compute.internal>
2020-02-13 11:26:05 +0900

    impala-state-store, impala-catlog host as parameter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,27 +4,27 @@ set -e
 function do_help {
   echo HELP:
   echo "Supported commands:"
-  echo "   master              - Start a Kudu Master"
-  echo "   tserver             - Start a Kudu TServer"
-  echo "   single              - Start a Kudu Master+TServer in one container"
-#  echo "   kudu                - Run the Kudu CLI"
-  echo "   help                - print useful information and exit"
+  echo "   hadoop-nm              - Start Hadoop Namenode"
+  echo "   hive-metastore         - Start Hive Metastore"
+  echo "   impala-state-store     - Start Impala Statestore"
+  echo "   impala-catalog         - Start Impala Catalog"
+  echo "   impala-server          - Start Impala Server and Hadoop Datanode"
+  echo "   standalone             - Start Standalone mode for Impala for Test"
+  echo "   help                   - print useful information and exit"
   echo ""
   echo "Other commands can be specified to run shell commands."
-  echo "Set the environment variable KUDU_OPTS to pass additional"
-  echo "arguments to the kudu process. DEFAULT_KUDU_OPTS contains"
+  #echo "Set the environment variable KUDU_OPTS to pass additional"
+  #echo "arguments to the kudu process. DEFAULT_KUDU_OPTS contains"
   echo "a recommended base set of options."
 
   exit 0
 }
 
-
-
-
 KUDU_MASTERS=${KUDU_MASTERS:=""}
 
-IMPALA_CATALOG_SERVICE_HOST=impala-catalog
-IMPALA_STATE_STORE_HOST=impala-state-store
+IMPALA_CATALOG_SERVICE_HOST=${IMPALA_CATALOG_SERVICE_HOST:="impala-catalog"}
+IMPALA_STATE_STORE_HOST=${IMPALA_STATE_STORE_HOST:="impala-state-store"}
+
 IMPALA_STATE_STORE_PORT=24000
 IMPALA_BACKEND_PORT=22000
 IMPALA_LOG_DIR=/var/log/impala
@@ -34,6 +34,7 @@ IMPALA_STATE_STORE_ARGS=" -log_dir=${IMPALA_LOG_DIR} -state_store_port=${IMPALA_
 
 
 chmod +x /wait-for-it.sh
+
 
 if [[ "$1" == "hadoop-nm" ]]; then
 
@@ -45,7 +46,7 @@ if [[ "$1" == "hadoop-nm" ]]; then
   sudo -u hdfs hdfs dfs -chmod 777 /
  
 elif [[ "$1" == "hive-metastore" ]]; then
-
+   # PostgreSQL depandency
    /wait-for-it.sh postgres:5432 -t 120
 
    psql -h postgres -U postgres -c "CREATE DATABASE metastore;" 2>/dev/null
@@ -55,10 +56,11 @@ elif [[ "$1" == "hive-metastore" ]]; then
    /etc/init.d/hive-metastore start
 
 elif [[ "$1" == "impala-state-store" ]]; then
-  #/etc/init.d/impala-state-store start
+   #/etc/init.d/impala-state-store start
    /bin/su -s /bin/bash -c "/bin/bash -c 'cd ~ && exec /usr/bin/statestored ${IMPALA_CATALOG_ARGS} >>${IMPALA_LOG_DIR}/impala-state-store.log 2>&1' &" impala
+
 elif [[ "$1" == "impala-catalog" ]]; then
-  
+  # Hive-Metastore depandency  
   /wait-for-it.sh hive-metastore:9083 -t 120
 
   #/etc/init.d/impala-catalog start
@@ -78,25 +80,24 @@ elif [[ "$1" == "impala-server" ]]; then
       -state_store_host=${IMPALA_STATE_STORE_HOST} \
       -be_port=${IMPALA_BACKEND_PORT} \
       -kudu_master_hosts=$KUDU_MASTERS"
-  else
+   else
     IMPALA_SERVER_ARGS=" \
       -log_dir=${IMPALA_LOG_DIR} \
       -catalog_service_host=${IMPALA_CATALOG_SERVICE_HOST} \
       -state_store_port=${IMPALA_STATE_STORE_PORT} \
       -state_store_host=${IMPALA_STATE_STORE_HOST} \
       -be_port=${IMPALA_BACKEND_PORT}"
-  fi
+   fi
 
 
-
-  /etc/init.d/hadoop-hdfs-datanode start
+   /etc/init.d/hadoop-hdfs-datanode start
   
-  #/etc/init.d/impala-server start
+   #/etc/init.d/impala-server start
    /bin/su -s /bin/bash -c "/bin/bash -c 'cd ~ && exec /usr/bin/impalad ${IMPALA_SERVER_ARGS} >>${IMPALA_LOG_DIR}/impala-server.log 2>&1' &" impala
 
 elif [[ "$1" == "standalone" ]]; then
 
-  echo "=========== Standalone(Test) ==============="
+  echo "=========== Standalone for Test ==============="
   
   echo "=========== HDFS start ==============="
   /etc/init.d/hadoop-hdfs-namenode start
@@ -122,12 +123,12 @@ elif [[ "$1" == "standalone" ]]; then
   echo "=========== Impala Service end ==============="
 
 
-
 elif [[ "$1" == "help" ]]; then
   print_help
   exit 0
 fi
 
-# Support calling anything else in the container.
+
+# Container Run without stopping
 tail -f /dev/null
 
